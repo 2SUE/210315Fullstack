f45a4d2291bf2ab55f9a9947f6c944c73f1194c5
2SUE <dbb025284@naver.com>
2021-04-14 12:34:22 +0900

    210414 java

--- chap01/src/sec06/ch07/MyArrayList.java
+++ chap01/src/sec06/ch07/MyArrayList.java
@@ -2,16 +2,17 @@ package sec06.ch07;
 
 public class MyArrayList implements MyList {
 
-	private int[] arr;
-	
+	// 전역변수는 무조건 디폴트 값이 들어감
+	private int[] arr; // = null
+
 	public MyArrayList() {
 		arr = new int[0];
 	}
-	
+
 	@Override
 	public void add(int value) {
 		int[] temp = new int[arr.length + 1];
-		for(int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			temp[i] = arr[i];
 		}
 		temp[arr.length] = value;
@@ -26,6 +27,73 @@ public class MyArrayList implements MyList {
 	@Override
 	public int get(int index) {
 		return arr[index];
+	}
+
+	public int remove() {
+		int[] temp = new int[arr.length - 1];
+		for (int i = 0; i < temp.length; i++) {
+			temp[i] = arr[i];
+		}
+
+		int lastVal = arr[arr.length - 1];
+		arr = temp;
+
+		return lastVal;
+	}
+
+	@Override
+	public void add(int index, int value) {
+		
+		int[] temp = new int[arr.length + 1];
+
+		/*
+		for (int i = 0; i < temp.length; i++) {
+			if (i < index) {
+				temp[i] = arr[i];
+			} else if (i == index) {
+				temp[i] = value;
+			} else {
+				temp[i] = arr[i -1]; 
+			}
+		}
+		*/
+		
+		for (int i = 0; i < arr.length; i++) {
+			int tIdx = i < index ? i : i+1;
+			temp[tIdx] = arr[i];
+		}
+		temp[index] = value;
+		
+		arr = temp;
+		
+	}
+
+	@Override
+	public int remove(int index) {
+		int[] temp = new int[arr.length-1];
+		
+		/*
+		int last = 0;
+		
+		for(int i = 0; i<temp.length; i++) {
+			if(i < index) {
+				temp[i] = arr[i];
+			} else if(i == index) {
+				last = arr[i];
+				temp[i] = arr[i+1];
+			} else {
+				temp[i] = arr[i+1];
+			}
+		}
+		*/
+		
+		int last = arr[index];
+		for(int i = 0; i<temp.length; i++) {
+			int tIdx = i < index ? i : i+1;
+			temp[i] = arr[tIdx];
+		}
+		arr = temp;
+		return last;
 	}
 
 }
--- chap01/src/sec06/ch07/MyList.java
+++ chap01/src/sec06/ch07/MyList.java
@@ -2,6 +2,9 @@ package sec06.ch07;
 
 public interface MyList {
 	/* public abstract */ void add(int value);
+	void add(int index, int value);
 	int size();
 	int get(int index);
+	int remove();
+	int remove(int index);
 }
