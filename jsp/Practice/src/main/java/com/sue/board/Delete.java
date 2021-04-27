9d792467ecaf223ea0de6b8d0daad874e4cbae37
2SUE <dbb025284@naver.com>
2021-04-28 00:19:50 +0900

    jsp project modify

--- jsp/Practice/src/main/java/com/sue/board/Delete.java
+++ jsp/Practice/src/main/java/com/sue/board/Delete.java
@@ -11,15 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/delete")
 public class Delete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String pageNum = request.getParameter("no");
 		Database.LIST.remove(Integer.parseInt(pageNum));
 		
 		response.sendRedirect("/list");
 	}
-
 }
