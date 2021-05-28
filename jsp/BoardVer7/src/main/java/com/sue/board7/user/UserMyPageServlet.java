fc005e37204ee2641cdc19d29a54ac65ecbd6c02
2SUE <dbb025284@naver.com>
2021-05-28 14:23:23 +0900

    210528 jsp + servlet + DB + Ajax

--- jsp/BoardVer7/src/main/java/com/sue/board7/user/UserMyPageServlet.java
+++ jsp/BoardVer7/src/main/java/com/sue/board7/user/UserMyPageServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import com.sue.board7.MyFileUtils;
 import com.sue.board7.MyUtils;
 
 @WebServlet("/user/myPage")
@@ -30,30 +31,53 @@ public class UserMyPageServlet extends HttpServlet {
 		
 		System.out.println("uploadPath : " + uploadPath);
 																										   // 같은 이름의 파일 넘버링		
-		MultipartRequest multi = new MultipartRequest(request, uploadPath + "/temp", maxFileSize, "UTF-8", new DefaultFileRenamePolicy());
-	
-		int loginUserPk = MyUtils.getLoginUserPK(request);
-		
-		String targetFolder = uploadPath + "/user/" + loginUserPk;
-		
-		File folder = new File(targetFolder);
-		
-		// 해당 폴더가 있으면 true 없으면 false
-		// mkdirs : 폴더 여러 개 생성 mkdir : 폴더 한 개 생성
-		folder.mkdirs(); 
-		
-		String fileNm = multi.getFilesystemName("profileImg"); // input name
-		System.out.println("fileNm : " + fileNm);
-		
-		// ~ 파일 확장자 구하기 ~ 
-		int lastDotIdx = fileNm.lastIndexOf("."); // 마지막 . 찾고 인덱스 번호 리턴
-		String ext = fileNm.substring(lastDotIdx); // 해당 인덱스부터 마지막 인덱스까지 
-		// String ext = fileNm.substring(fileNm.lastIndexOf("."));
+		try {
+			MultipartRequest multi = new MultipartRequest(request, uploadPath + "/temp", maxFileSize, "UTF-8", new DefaultFileRenamePolicy());
+			
+			UserEntity loginUser = MyUtils.getLoginUser(request);
+			
+			String targetFolder = uploadPath + "/user/" + loginUser.getIuser();
+			
+			// 폴더 삭제 메소드 : 폴더를 안 지우면 프로필 사진을 업데이트 할때마다 쌓임
+			MyFileUtils.delFolder(targetFolder); 
+			
+			File folder = new File(targetFolder);
+			
+			
+			// 해당 폴더가 있으면 true 없으면 false
+			// mkdirs : 폴더 여러 개 생성 mkdir : 폴더 한 개 생성
+			folder.mkdirs();
+			
+			String fileNm = multi.getFilesystemName("profileImg"); // input name
+			System.out.println("fileNm: " + fileNm);
+			
+			if(fileNm == null) {
+				doGet(request, response);
+				return;
+			}
+			
+			// ~ 파일 확장자 구하기 ~ 
+			int lastDotIdx = fileNm.lastIndexOf("."); // 마지막 . 찾고 인덱스 번호 리턴
+			String ext = fileNm.substring(lastDotIdx); // 해당 인덱스부터 마지막 인덱스까지 
+			// String ext = fileNm.substring(fileNm.lastIndexOf("."));
 
-		// UUID 값 (랜덤 값 생성, 중복된 값 안 나온다~~~~)
-		String newFileNm = UUID.randomUUID().toString() + ext;
+			// UUID 값 (랜덤 값 생성, 중복된 값 안 나온다~~~~)
+			String newFileNm = UUID.randomUUID().toString() + ext;
+			
+			File imgFile = new File(uploadPath + "/temp/" + fileNm);
+			imgFile.renameTo(new File(targetFolder + "/" + newFileNm));
+			
+			UserEntity param = new UserEntity();
+			param.setIuser(loginUser.getIuser());
+			param.setProfileImg(newFileNm);
+			
+			UserDAO.updUser(param);
+
+			loginUser.setProfileImg(newFileNm); // 세션에 있는 loginUser의 프로필 수정
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		File imgFile = new File(uploadPath + "/temp" + "/" + fileNm);
-		imgFile.renameTo(new File(targetFolder + "/" + newFileNm));
+		doGet(request, response);
 	}
 }
