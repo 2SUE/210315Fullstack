6d3879e4eb6f408ac1b661f69cfbdc2a4e59ce3c
2SUE <dbb025284@naver.com>
2021-06-07 17:00:10 +0900

    210607 spring

---
+++ spring/spring1/src/main/java/com/sue/spring/board/BoardCmtDomain.java
@@ -0,0 +1,11 @@
+package com.sue.spring.board;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+public class BoardCmtDomain  extends BoardEntity {
+    private String writerNm;
+    private String profileImg;
+}
